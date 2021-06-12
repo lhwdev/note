9c212901d553e3d0b698ad7e637c34e2b213e7e7
lhwdev <lhwdev6@outlook.com>
2021-06-12 20:20:04 +0900

    Add exception catching

--- macros/main.py
+++ macros/main.py
@@ -2,7 +2,9 @@
 
 def on_pre_page_macros(env):
 	global meta_date
-	meta_date = env.page.meta['date']
+	if 'date' in env.page.meta:
+		meta_date = env.page.meta['date']
+	else: meta_date = ''
 
 
 def define_env(env):
