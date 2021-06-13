b77939594363baf6ad645f79b999f7250b641afd
lhwdev <lhwdev6@outlook.com>
2021-06-14 07:52:09 +0900

    Change head() design

--- macros/main.py
+++ macros/main.py
@@ -9,4 +9,4 @@ def define_env(env):
 	
 	@env.macro
 	def head():
-		return f'''<div style="color: var(--md-primary-fg-color--light); font-size: small;">{env_meta.get('date', '')}</div>'''
+		return f'''<div style="color: var(--md-default-fg-color--light); font-size: small; padding: -4px 0 12px 0;">{env_meta.get('date', '')}</div>'''
